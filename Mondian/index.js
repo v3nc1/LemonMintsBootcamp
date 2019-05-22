0aaf6f282916d7fd5861b9ebf47e62e97aee5038
Venci <venci@localhost.localdomain>
2019-05-22 19:06:30 +0200

    random Mondian added

--- Mondian/index.js
+++ Mondian/index.js
@@ -1,16 +1,20 @@
-const colors = ['red', 'blue', 'yellow', 'white', 'black'];
+const colors = ['white', 'red', 'blue', 'yellow', 'black'];
+const minR = 0;
+const maxR = 100;
+let min;
+let max;
+let random;
+let randomR;
 
 function randomNumber() {
-    const minR = 0;
-    const maxR = 5;
-    let randomR = Math.floor(Math.random() * (+maxR - +minR)) + +minR;
+    randomR = Math.floor(Math.random() * (+maxR - +minR)) + +minR;
     document.getElementById("number").innerHTML = randomR;
 }
 
 function randomNumberBetween(low, hi) {
-    let min = low;
-    let max = hi;
-    let random = Math.floor(Math.random() * (+max - +min)) + +min;
+    min = low;
+    max = hi;
+    random = Math.floor(Math.random() * (+max - +min)) + +min;
     return random;
 }
 
@@ -19,37 +23,53 @@ function getColor() {
     return colors[randomNumberBetween(0, 4)];
 }
 
+function refresh() {
+
+    document.location.reload();
+
+}
+
 function addDiv() {
 
+
     let div1;
-    let flexDiv;
+    let flexDiv = document.createElement('DIV');
+    let newFlexDiv;
     let flex = document.getElementById('flex');
 
-    for (let a = 0; a <= randomNumberBetween(0, 10); a++) {
 
-        // flexDiv = document.createElement('DIV');
-        // flexDiv.style.display = 'flex';
-        // flexDiv.style.alignContent = 'flex-start';
-        // flexDiv.style.backgroundColor = 'black';
+    for (let b = 0; b <= randomNumberBetween(1, 7); b++) {
+        newFlexDiv = document.createElement('DIV');
+        newFlexDiv.style.backgroundColor = 'black';
+        newFlexDiv.style.display = 'flex';
+        newFlexDiv.style.height = '250px';
+        newFlexDiv.style.alignContent = 'flex-start';
+        newFlexDiv.style.backgroundColor = 'black';
+        newFlexDiv.style.flexDirection = 'row';
+        if (randomNumberBetween(1, 10) % 2 === 0) {
 
-        // document.body.appendChild(flex);
-        // flex.style.height = randomNumberBetween(20, 250);
-        // flex.style.alignItems = 'stretch';
-
-        for (let i = 0; i <= randomNumberBetween(0, 10); i++) {
-
-            div1 = document.createElement('DIV');
-            div1.style.backgroundColor = getColor();
-            div1.style.flexGrow = randomNumberBetween(0, 10);
-            div1.style.height = randomNumberBetween(20, 350);
-            div1.style.width = randomNumberBetween(20, 250);
-            div1.style.lineBreak = 'after';
-
-
-
+            newFlexDiv.style.flexDirection = 'column';
         }
-        flex.appendChild(div1);
+        for (let a = 0; a <= randomNumberBetween(1, 7); a++) {
 
+            for (let i = 0; i <= randomNumberBetween(1, 7); i++) {
+
+                div1 = document.createElement('DIV');
+                div1.style.margin = '1.5px';
+                div1.style.backgroundColor = getColor();
+                div1.style.flexGrow = randomNumberBetween(0, 10);
+
+                document.body.appendChild(newFlexDiv);
+
+
+                newFlexDiv.appendChild(div1);
+            }
+            document.body.appendChild(newFlexDiv);
+        }
+
+
+
+        document.body.appendChild(newFlexDiv);
     }
 
 
