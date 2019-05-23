e5e75636b1ae9c09bf7afccdd3c2594b61f963a9
venci-ivos <venci-ivos@users.noreply.github.com>
2019-05-23 16:42:20 +0200

    jej

--- Mondian/index.js
+++ Mondian/index.js
@@ -1,4 +1,4 @@
-const colors = ['white', 'red', 'blue', 'yellow', 'black'];
+const colors = ['white', 'red', 'blue', 'white', 'yellow', 'black', 'white', 'white'];
 const minR = 0;
 const maxR = 100;
 let min;
@@ -20,7 +20,7 @@ function randomNumberBetween(low, hi) {
 
 function getColor() {
 
-    return colors[randomNumberBetween(0, 4)];
+    return colors[randomNumberBetween(0, 8)];
 }
 
 function refresh() {
@@ -31,47 +31,27 @@ function refresh() {
 
 function addDiv() {
 
-
     let div1;
-    let flexDiv = document.createElement('DIV');
     let newFlexDiv;
-    let flex = document.getElementById('flex');
 
-
-    for (let b = 0; b <= randomNumberBetween(1, 7); b++) {
+    for (let b = 0; b <= randomNumberBetween(3, 7); b++) {
         newFlexDiv = document.createElement('DIV');
         newFlexDiv.style.backgroundColor = 'black';
         newFlexDiv.style.display = 'flex';
-        newFlexDiv.style.height = '250px';
-        newFlexDiv.style.alignContent = 'flex-start';
-        newFlexDiv.style.backgroundColor = 'black';
-        newFlexDiv.style.flexDirection = 'row';
-        if (randomNumberBetween(1, 10) % 2 === 0) {
-
-            newFlexDiv.style.flexDirection = 'column';
-        }
-        for (let a = 0; a <= randomNumberBetween(1, 7); a++) {
-
-            for (let i = 0; i <= randomNumberBetween(1, 7); i++) {
+        newFlexDiv.style.width = '100%';
+        newFlexDiv.style.height = randomNumberBetween(10, 250) + 'px';
+        for (let a = 0; a <= randomNumberBetween(0, 7); a++) {
+            for (let i = 0; i <= randomNumberBetween(0, 7); i++) {
 
                 div1 = document.createElement('DIV');
                 div1.style.margin = '1.5px';
                 div1.style.backgroundColor = getColor();
                 div1.style.flexGrow = randomNumberBetween(0, 10);
-
                 document.body.appendChild(newFlexDiv);
-
-
                 newFlexDiv.appendChild(div1);
             }
             document.body.appendChild(newFlexDiv);
         }
 
-
-
-        document.body.appendChild(newFlexDiv);
     }
-
-
-
 }
